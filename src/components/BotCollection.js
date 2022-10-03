f34f8595eaf357fd2bc7eee6219924ac808302bd
D-Micheni <dennis.micheni@student.moringaschool.com>
2022-10-03 13:20:05 +0300

    src/components/BotCollection.js

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import BotCard from "./BotCard";
+import BotSpecs from "./BotSpecs";
 
-function BotCollection() {
+function BotCollection({bots,getArmy,handleDel,removeSelected}) {
   // Your code here
+  const[list, setList]=useState(false)
+  const[viewarmy, setViewArmy]=useState([])
+  console.log(typeof(viewarmy))
+
+  function getClickedDetails(obj){
+    setViewArmy([obj])
+    setList(!list)
+    console.log(obj)
+    
+  }
+
+  const botContainer = bots.map((bot) => {
+  return <BotCard bot={bot} key={bot.id} handleDel={handleDel} getClickedDetails={getClickedDetails} setList={setList} list={list}/>
+  });
+
+  const botList = viewarmy.map((bot) => {
+      return <BotSpecs bot={bot} getArmy={getArmy} setViewArmy={setViewArmy} setList={setList} list={list} />
+    });
   return (
     <div className="ui four column grid">
-      <div className="row">
-        {/*...and here..*/}
-        Collection of all bots
-      </div>
+      <div className="row">{list ? botList:botContainer}</div>
     </div>
   );
 }
 
 export default BotCollection;
+
