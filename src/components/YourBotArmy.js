b5b1ff4bea07dca81bb97d697394b9d94e9be950
D-Micheni <dennis.micheni@student.moringaschool.com>
2022-10-03 13:08:39 +0300

    src/components/YourBotArmy.js

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,14 +1,19 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function YourBotArmy() {
+function YourBotArmy({army ,show ,removeSelected}) {
   //your bot army code here...
+  const botContainer = army.map((bot) => {
+    return <BotCard bot={bot} key={bot.id} show={show} removeSelected={removeSelected}/>;
+  });
 
   return (
     <div className="ui segment inverted olive bot-army">
       <div className="ui five column grid">
         <div className="row bot-army-row">
           {/*...and here...*/}
-          Your Bot Army
+          {botContainer}
+
         </div>
       </div>
     </div>
