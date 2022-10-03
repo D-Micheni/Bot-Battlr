e62536f8b48ffbc61d4c5ddd4ec54ba51f3f3072
D-Micheni <dennis.micheni@student.moringaschool.com>
2022-10-03 13:43:17 +0300

    src/components/BotCard.js

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,4 +1,5 @@
-import React from "react";
+import { set } from "date-fns";
+import React, { useState } from "react";
 
 const botTypeClasses = {
   Assault: "icon military",
@@ -9,14 +10,37 @@ const botTypeClasses = {
   Captain: "icon star",
 };
 
-function BotCard({ bot }) {
+function BotCard({ bot,show ,handleDel,removeSelected,getClickedDetails,setList,list }) {
+
+  function OnDel(id) {
+    fetch(`http://localhost:8002/bots/${id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => handleDel(id));
+    console.log("to delete" + id);
+
+  }
+  function delSoldier(id){
+    console.log('hh')
+    setList(!list)
+    console.log('tunakutoa soon' + id)
+
+    removeSelected(id)
+    
+  }
+function MyObj(obj){
+  getClickedDetails(obj)
+}
+
+
+  // function AddArmy(my_army) {
+  //   getArmy(my_army);
+  // }
+  // console.log(bot)
   return (
     <div className="ui column">
-      <div
-        className="ui card"
-        key={bot.id}
-        onClick={() => console.log("add code to connect event listener")}
-      >
+      <div className="ui card" key={bot.id} onClick={() =>{ show ? delSoldier(bot.id) :MyObj(bot)}}>
         <div className="image">
           <img alt="oh no!" src={bot.avatar_url} />
         </div>
@@ -45,14 +69,17 @@ function BotCard({ bot }) {
           </span>
           <span>
             <div className="ui center aligned segment basic">
-              <button
-                className="ui mini red button"
-                onClick={() =>
-                  console.log("add code to connect event listener")
-                }
-              >
-                x
-              </button>
+              {show ? null :
+                <button
+                  className="ui mini red button"
+                  onClick={() =>{OnDel(bot.id)}
+
+                    // console.log("add code to connect event listener")
+                  }
+                >
+                  x
+                </button>
+              }
             </div>
           </span>
         </div>
